1b37b1b1f1449c0fb4f872a0ab91043275f5c63f
Dalgona <dalgona@hontou.moe>
2019-09-18 21:31:54 +0900

    Implement the composite glyph builder DSL

---
+++ lib/ttf_lib/composite_glyphs.ex
@@ -0,0 +1,50 @@
+defmodule TTFLib.CompositeGlyphs do
+  defmacro __using__(_opts) do
+    quote do
+      import unquote(__MODULE__)
+
+      @glyphs []
+    end
+  end
+
+  defmacro glyph([{type, id}], [do: block]) when type in ~w(unicode name)a do
+    item =
+      quote do
+        %{
+          type: unquote(type),
+          id: unquote(id)
+        }
+      end
+
+    quote do
+      @components []
+
+      unquote(block)
+
+      @glyphs [Map.put(unquote(item), :components, @components) | @glyphs]
+    end
+  end
+
+  defmacro component(glyph_id, x_off, y_off) do
+    component_expr =
+      quote do
+        %{
+          glyph: unquote(glyph_id),
+          x_offset: unquote(x_off),
+          y_offset: unquote(y_off)
+        }
+      end
+
+    quote do
+      @components @components ++ [unquote(component_expr)]
+    end
+  end
+
+  defmacro export_glyphs do
+    quote do
+      @glyphs Enum.reverse(@glyphs)
+
+      def glyphs, do: @glyphs
+    end
+  end
+end
