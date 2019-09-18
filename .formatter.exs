e0fd55715923938c3f38ae04f8abed8bf1fe20ba
Dalgona <dalgona@hontou.moe>
2019-09-18 23:10:12 +0900

    Add .formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,13 @@
+[
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    glyph: :"*",
+    component: 3,
+    advance: 1,
+    xmin: 1,
+    xmax: 1,
+    ymin: 1,
+    ymax: 1,
+    data: 1
+  ]
+]
