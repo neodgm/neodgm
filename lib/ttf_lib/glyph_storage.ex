57b26cd95e49188ff4b45feafae357eb2dc723be
Dalgona <dalgona@hontou.moe>
2019-09-21 09:52:46 +0900

    Link composite glyphs when loading glyphs

--- lib/ttf_lib/glyph_storage.ex
+++ lib/ttf_lib/glyph_storage.ex
@@ -15,18 +15,17 @@ defmodule TTFLib.GlyphStorage do
 
   @impl GenServer
   def init(glyph_sources) do
-    loaded_glyphs =
+    glyphs =
       glyph_sources
       |> Enum.map(& &1.glyphs)
       |> List.flatten()
       |> set_glyph_index([], 0)
 
-    glyph_lookup =
-      loaded_glyphs
-      |> Enum.map(&{{&1.type, &1.id}, &1})
-      |> Map.new()
+    tmp_lookup = make_lookup(glyphs)
+    linked = Enum.map(glyphs, &link_composite(&1, tmp_lookup))
+    lookup = make_lookup(linked)
 
-    {:ok, {loaded_glyphs, glyph_lookup}}
+    {:ok, {linked, lookup}}
   end
 
   @impl GenServer
@@ -40,10 +39,28 @@ defmodule TTFLib.GlyphStorage do
     {:reply, map[id], state}
   end
 
+  defp make_lookup(glyphs) do
+    glyphs
+    |> Enum.map(&{{&1.type, &1.id}, &1})
+    |> Map.new()
+  end
+
   defp set_glyph_index(glyphs, acc, index)
   defp set_glyph_index([], acc, _), do: Enum.reverse(acc)
 
   defp set_glyph_index([g | gs], acc, index) do
     set_glyph_index(gs, [Map.put(g, :index, index) | acc], index + 1)
+  end
+
+  defp link_composite(glyph, lookup)
+  defp link_composite(%{contours: _} = simple, _lookup), do: simple
+
+  defp link_composite(%{components: components} = composite, lookup) do
+    linked_components =
+      Enum.map(components, fn %{glyph: glyph_id} = component ->
+        %{component | glyph: lookup[glyph_id]}
+      end)
+
+    %{composite | components: linked_components}
   end
 end
