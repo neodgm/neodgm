bd3bb7e3c1bb666a787062402bb512716dc05489
Dalgona <dalgona@hontou.moe>
2019-09-25 08:16:45 +0900

    Add NeoDGM.NameTable

---
+++ lib/neodgm/name_table.ex
@@ -0,0 +1,63 @@
+defmodule NeoDGM.NameTable do
+  require TTFLib.NameTableBuilder
+  import TTFLib.NameTableBuilder
+
+  ofl = File.read!(Path.join(File.cwd!(), "ofl_raw.txt"))
+
+  copyright_en = """
+  Original font was released under the public domain by Jungtae Kim \
+  in 1990s.
+  Conversion and additional character design by Dalgona. \
+  <dalgona@hontou.moe>
+  """
+
+  copyright_ko = """
+  원본 글꼴은 1990년대에 김중태 한글문화원 원장에 의해 퍼블릭 \
+  도메인으로 공개되었습니다.
+  변환 및 추가적인 문자 디자인: Dalgona. <dalgona@hontou.moe>
+  """
+
+  name_table do
+    platform :macintosh do
+      encoding :roman do
+        language :en do
+          copyright copyright_en
+          family "NeoDunggeunmo"
+          subfamily "Regular"
+          full_name "NeoDunggeunmo Regular"
+          version "Version 1.20"
+          postscript_name "NeoDunggeunmo-Regular"
+          license ofl
+          license_url "https://scripts.sil.org/OFL"
+        end
+      end
+    end
+
+    platform :windows do
+      encoding :unicode_bmp do
+        language :en_us do
+          copyright copyright_en
+          unique_id "Dalgona : NeoDunggeunmo : 2019"
+          family "NeoDunggeunmo"
+          subfamily "Regular"
+          full_name "NeoDunggeunmo Regular"
+          version "Version 1.20"
+          postscript_name "NeoDunggeunmo-Regular"
+          license ofl
+          license_url "https://scripts.sil.org/OFL"
+        end
+
+        language :ko_kr do
+          copyright copyright_ko
+          family "Neo둥근모"
+          subfamily "보통"
+          full_name "Neo둥근모 보통"
+          version "Version 1.20"
+          postscript_name "NeoDunggeunmo-Regular"
+          license ofl
+          license_url "https://scripts.sil.org/OFL"
+        end
+      end
+    end
+  end
+end
