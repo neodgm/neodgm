47bb12e2bf89b0585b429f9e79839ca568733f07
Dalgona <dalgona@hontou.moe>
2019-09-29 10:25:32 +0900

    Implement compilation of LookupList and Lookup
    Compilation of subtables is delegated to lookup owners.

---
+++ lib/ttf_lib/table_source/otf_layout/lookup.ex
@@ -0,0 +1,38 @@
+defmodule TTFLib.TableSource.OTFLayout.Lookup do
+  defstruct ~w(owner type name subtables)a
+
+  @type t :: %__MODULE__{
+          owner: module(),
+          type: integer(),
+          name: term(),
+          subtables: [map()]
+        }
+
+  @spec compile(t()) :: binary()
+  def compile(%{subtables: subtables} = lookup) do
+    subtable_count = length(subtables)
+
+    # Size of MarkFilteringSet: 0
+    offset_base = 6 + subtable_count * 2 + 0
+
+    {_, offsets, compiled_subtables} =
+      Enum.reduce(subtables, {offset_base, [], []}, fn subtable, {pos, offsets, tables} ->
+        compiled = lookup.owner.compile_subtable(subtable, lookup.type)
+
+        {pos + byte_size(compiled), [pos | offsets], [compiled | tables]}
+      end)
+
+    data = [
+      <<lookup.type::16>>,
+      # LookupFlag (Not used yet)
+      <<0::16>>,
+      <<subtable_count::16>>,
+      offsets |> Enum.reverse() |> Enum.map(&<<&1::16>>),
+      # MarkFilteringSet (Not present)
+      "",
+      Enum.reverse(compiled_subtables)
+    ]
+
+    IO.iodata_to_binary(data)
+  end
+end
--- lib/ttf_lib/table_source/otf_layout_tables.ex
+++ lib/ttf_lib/table_source/otf_layout_tables.ex
@@ -64,15 +64,25 @@ defmodule TTFLib.TableSource.OTFLayout.LookupList do
   defstruct ~w(lookups)a
 
   @type t :: %__MODULE__{lookups: [Lookup.t()]}
-end
 
-defmodule TTFLib.TableSource.OTFLayout.Lookup do
-  defstruct ~w(owner type name subtables)a
+  @spec compile(t()) :: binary()
+  def compile(%{lookups: lookups}) do
+    lookup_count = length(lookups)
+    offset_base = 2 + lookup_count * 2
 
-  @type t :: %__MODULE__{
-          owner: module(),
-          type: integer(),
-          name: term(),
-          subtables: [map()]
-        }
+    {_, offsets, tables} =
+      Enum.reduce(lookups, {0, [], []}, fn lookup, {pos, offsets, tables} ->
+        table = Lookup.compile(lookup)
+
+        {pos + byte_size(table), [offset_base + pos | offsets], [table | tables]}
+      end)
+
+    data = [
+      <<lookup_count::16>>,
+      offsets |> Enum.reverse() |> Enum.map(&<<&1::16>>),
+      Enum.reverse(tables)
+    ]
+
+    IO.iodata_to_binary(data)
+  end
 end
