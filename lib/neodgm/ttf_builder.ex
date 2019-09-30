3181f34cab20769be9a7c965c16e81ada4484c84
Dalgona <dalgona@hontou.moe>
2019-09-30 23:53:26 +0900

    Extract TTFLib.Builder module

--- lib/neodgm/ttf_builder.ex
+++ lib/neodgm/ttf_builder.ex
@@ -1,7 +1,6 @@
 defmodule NeoDGM.TTFBuilder do
   alias NeoDGM.NameTable
-  alias TTFLib.GlyphStorage
-  alias TTFLib.TableSource.{Cmap, Glyf, GSUB, Head, Hmtx, Maxp, Name, OS_2, Post}
+  alias TTFLib.Builder
 
   @version Version.parse!(Mix.Project.config()[:version])
 
@@ -54,81 +53,19 @@ defmodule NeoDGM.TTFBuilder do
       cap_height: 10
     }
 
-    {:ok, _} = GlyphStorage.start_link(@glyph_sources)
+    params = %{
+      glyph_sources: @glyph_sources,
+      gsub: NeoDGM.GSUB.gsub(),
+      metrics: metrics,
+      os_2: os_2,
+      version: @version,
+      name_table: NameTable.name_table()
+    }
 
-    glyf = Glyf.generate()
-    hmtx = Hmtx.generate()
-    maxp = Maxp.generate()
-    gsub = NeoDGM.GSUB.gsub()
-
-    compiled_tables = [
-      Glyf.compile(glyf),
-      Hmtx.compile(hmtx, metrics),
-      Maxp.compile(maxp),
-      Name.compile(NameTable.name_table(), 0),
-      Cmap.compile(),
-      Post.compile(metrics, 2),
-      OS_2.compile(os_2, metrics, 4),
-      GSUB.compile(gsub)
-    ]
-
-    :ok = GenServer.stop(GlyphStorage)
-
-    head = Head.compile(@version, glyf, metrics)
-    tables = sort_tables([head, compiled_tables])
-    preamble = make_preamble(tables)
-    table_record = make_table_record(tables)
-    temp_ttf = make_ttf(preamble, table_record, tables)
-
-    new_head = Head.update_checksum_adjustment(head, temp_ttf)
-    new_tables = sort_tables([new_head, compiled_tables])
-    ttf = make_ttf(preamble, table_record, new_tables)
+    ttf = Builder.build_ttf(params)
 
     "neodgm.ttf"
     |> Path.expand(File.cwd!())
     |> File.open!([:write, :binary], &IO.binwrite(&1, ttf))
-  end
-
-  defp sort_tables(compiled_tables) do
-    compiled_tables
-    |> List.flatten()
-    |> Enum.sort(&(&1.tag < &2.tag))
-  end
-
-  defp make_preamble(compiled_tables) do
-    num_tables = length(compiled_tables)
-    entry_selector = num_tables |> :math.log2() |> trunc()
-    search_range = (2 |> :math.pow(entry_selector) |> trunc()) * 16
-    range_shift = num_tables * 16 - search_range
-
-    [
-      <<0, 1, 0, 0>>,
-      <<num_tables::16>>,
-      <<search_range::16>>,
-      <<entry_selector::16>>,
-      <<range_shift::16>>
-    ]
-  end
-
-  defp make_table_record(compiled_tables) do
-    offset_base = 12 + length(compiled_tables) * 16
-
-    compiled_tables
-    |> Enum.reduce({0, []}, fn table, {pos, entries} ->
-      entry = [
-        table.tag,
-        <<table.checksum::32>>,
-        <<pos + offset_base::32>>,
-        <<table.real_size::32>>
-      ]
-
-      {pos + table.padded_size, [entry | entries]}
-    end)
-    |> elem(1)
-    |> Enum.reverse()
-  end
-
-  defp make_ttf(preamble, table_record, tables) do
-    IO.iodata_to_binary([preamble, table_record, Enum.map(tables, & &1.data)])
   end
 end
