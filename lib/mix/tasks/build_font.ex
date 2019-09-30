0456f2cb7429af9448a0abf2c6cfee2ca5675c44
Dalgona <dalgona@hontou.moe>
2019-10-01 00:20:00 +0900

    Replace NeoDGM.TTFBuilder with "mix build_font" Mix task

--- lib/mix/tasks/build_font.ex
+++ lib/mix/tasks/build_font.ex
@@ -1,5 +1,16 @@
 defmodule Mix.Tasks.BuildFont do
   use Mix.Task
+  alias NeoDGM.Params
+  alias TTFLib.Builder
 
-  def run(_args), do: NeoDGM.TTFBuilder.build_font()
+  def run(args) do
+    options = [variant: :string]
+    {parsed, _args} = OptionParser.parse!(args, switches: options)
+    params = Params.params(variant: parsed[:variant])
+    ttf = Builder.build_ttf(params)
+
+    "neodgm.ttf"
+    |> Path.expand(File.cwd!())
+    |> File.open!([:write, :binary], &IO.binwrite(&1, ttf))
+  end
 end
