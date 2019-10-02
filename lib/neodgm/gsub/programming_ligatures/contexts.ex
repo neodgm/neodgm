d43f08dd5ef63e59559353cc0d6468bc4a57e718
Dalgona <dalgona@hontou.moe>
2019-10-02 22:19:33 +0900

    Move GSUB lookups to separate modules

---
+++ lib/neodgm/gsub/programming_ligatures/contexts.ex
@@ -0,0 +1,187 @@
+defmodule NeoDGM.GSUB.ProgrammingLigatures.Contexts do
+  alias TTFLib.TableSource.GSUB
+  alias TTFLib.TableSource.OTFLayout.Lookup
+
+  def data do
+    [
+      %Lookup{
+        owner: GSUB,
+        type: 6,
+        name: "Short bidirectional arrow chain",
+        subtables: [
+          %{
+            format: 3,
+            backtrack: ['<'],
+            input: ['-='],
+            lookahead: ['>'],
+            substitutions: [
+              {0, "Short bidirectional arrow body"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 6,
+        name: "Left arrow head chain",
+        subtables: [
+          %{
+            format: 3,
+            backtrack: ['<'],
+            input: ['-'],
+            lookahead: [],
+            substitutions: [
+              {0, "Left arrow head"}
+            ]
+          },
+          %{
+            format: 3,
+            backtrack: ['<'],
+            input: ['='],
+            lookahead: ['='],
+            substitutions: [
+              {0, "Left arrow head"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 6,
+        name: "Right arrow head chain",
+        subtables: [
+          %{
+            format: 3,
+            backtrack: [["hyphen.larr.head", "equal.larr.head"], '<'],
+            input: ['-='],
+            lookahead: ['>'],
+            substitutions: [
+              {0, "Right arrow head alt"}
+            ]
+          },
+          %{
+            format: 3,
+            backtrack: [],
+            input: ['-='],
+            lookahead: ['>'],
+            substitutions: [
+              {0, "Right arrow head"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 6,
+        name: "Left arrow body chain",
+        subtables: [
+          %{
+            format: 3,
+            backtrack: [["hyphen.larr.head", "equal.larr.head"], '<'],
+            input: ['-='],
+            lookahead: ['-='],
+            substitutions: [
+              {0, "Left arrow body"}
+            ]
+          },
+          %{
+            format: 3,
+            backtrack: [["hyphen.larr.body", "equal.larr.body"]],
+            input: ['-='],
+            lookahead: ['-='],
+            substitutions: [
+              {0, "Left arrow body"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 6,
+        name: "Bidirectional arrow joiner chain",
+        subtables: [
+          %{
+            format: 3,
+            backtrack: [
+              [
+                "hyphen.larr.head",
+                "hyphen.larr.body",
+                "equal.larr.head",
+                "equal.larr.body"
+              ]
+            ],
+            input: ['-='],
+            lookahead: [["hyphen.rarr.head", "equal.rarr.head"], '>'],
+            substitutions: [
+              {0, "Bidirectional arrow joiner"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 6,
+        name: "Left arrow tail chain",
+        subtables: [
+          %{
+            format: 3,
+            backtrack: [
+              [
+                "hyphen.larr.head",
+                "hyphen.larr.body",
+                "equal.larr.head",
+                "equal.larr.body"
+              ]
+            ],
+            input: ['-='],
+            lookahead: [],
+            substitutions: [
+              {0, "Left arrow body"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 8,
+        name: "Right arrow body chain",
+        subtables: [
+          %{
+            format: 1,
+            backtrack: [],
+            lookahead: [
+              [
+                "hyphen.rarr.head",
+                "hyphen.rarr.body",
+                "equal.rarr.head",
+                "equal.rarr.body"
+              ]
+            ],
+            substitutions: [
+              {?-, "hyphen.rarr.body"},
+              {?=, "equal.rarr.body"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 6,
+        name: "Markup comment chain",
+        subtables: [
+          %{
+            format: 3,
+            backtrack: ['<'],
+            input: ['!', '-', '-'],
+            lookahead: [],
+            substitutions: [
+              {0, "Markup comment exclam"},
+              {1, "Left arrow body"},
+              {2, "Left arrow body"}
+            ]
+          }
+        ]
+      }
+    ]
+  end
+end
---
+++ lib/neodgm/gsub/programming_ligatures/substitutions.ex
@@ -0,0 +1,120 @@
+defmodule NeoDGM.GSUB.ProgrammingLigatures.Substitutions do
+  alias TTFLib.TableSource.GSUB
+  alias TTFLib.TableSource.OTFLayout.Lookup
+
+  def data do
+    [
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Short bidirectional arrow body",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?-, "hyphen.bidiarr.short"},
+              {?=, "equal.bidiarr.short"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Bidirectional arrow joiner",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?-, "hyphen.bidiarr.joiner"},
+              {?=, "equal.bidiarr.joiner"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Left arrow head",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?-, "hyphen.larr.head"},
+              {?=, "equal.larr.head"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Left arrow body",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?-, "hyphen.larr.body"},
+              {?=, "equal.larr.body"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Right arrow head",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?-, "hyphen.rarr.head"},
+              {?=, "equal.rarr.head"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Right arrow head alt",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?-, "hyphen.rarr.head2"},
+              {?=, "equal.rarr.head2"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Right arrow body",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?-, "hyphen.rarr.body"},
+              {?=, "equal.rarr.body"}
+            ]
+          }
+        ]
+      },
+      %Lookup{
+        owner: GSUB,
+        type: 1,
+        name: "Markup comment exclam",
+        subtables: [
+          %{
+            format: 2,
+            substitutions: [
+              {?!, "exclam.markupcomment"}
+            ]
+          }
+        ]
+      }
+    ]
+  end
+end
