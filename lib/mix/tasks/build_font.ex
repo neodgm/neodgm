6961b23b2a0a908b86374992a2e7b84df62c8a71
Dalgona <dalgona@hontou.moe>
2019-10-03 12:34:34 +0900

    Use different TTF file name for each variant

--- lib/mix/tasks/build_font.ex
+++ lib/mix/tasks/build_font.ex
@@ -3,13 +3,23 @@ defmodule Mix.Tasks.BuildFont do
   alias NeoDGM.Params
   alias TTFLib.Builder
 
+  @defined_variants ~w(code)
+
   def run(args) do
     options = [variant: :string]
     {parsed, _args} = OptionParser.parse!(args, switches: options)
-    params = Params.params(variant: parsed[:variant])
+    variant = parsed[:variant]
+
+    params = Params.params(variant: variant)
     ttf = Builder.build_ttf(params)
 
-    "neodgm.ttf"
+    suffix =
+      case variant do
+        variant when variant in @defined_variants -> "_#{variant}"
+        _ -> ""
+      end
+
+    "neodgm#{suffix}.ttf"
     |> Path.expand(File.cwd!())
     |> File.open!([:write, :binary], &IO.binwrite(&1, ttf))
   end
