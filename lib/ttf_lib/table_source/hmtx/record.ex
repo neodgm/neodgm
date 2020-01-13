8fa8b4c4753318c610678b117c88a4fed02d104d
Dalgona <dalgona@hontou.moe>
2020-01-13 19:08:32 +0900

    Generate correct hmtx record for composite glyphs
    where each component have non-zero X offset

--- lib/ttf_lib/table_source/hmtx/record.ex
+++ lib/ttf_lib/table_source/hmtx/record.ex
@@ -23,10 +23,18 @@ defmodule TTFLib.TableSource.Hmtx.Record do
   end
 
   def new(%{components: components}) do
-    ref_glyphs = Enum.map(components, & &1.glyph)
-    advance = ref_glyphs |> Enum.map(& &1.advance) |> Enum.max(fn -> 0 end)
-    xmin = ref_glyphs |> Enum.map(& &1.xmin) |> Enum.min(fn -> 0 end)
-    xmax = ref_glyphs |> Enum.map(& &1.xmax) |> Enum.max(fn -> 0 end)
+    metrics =
+      Enum.map(components, fn %{glyph: glyph, x_offset: xoff} ->
+        %{
+          advance: glyph.advance,
+          xmin: glyph.xmin + xoff,
+          xmax: glyph.xmax + xoff
+        }
+      end)
+
+    advance = metrics |> Enum.map(& &1.advance) |> Enum.max(fn -> 0 end)
+    xmin = metrics |> Enum.map(& &1.xmin) |> Enum.min(fn -> 0 end)
+    xmax = metrics |> Enum.map(& &1.xmax) |> Enum.max(fn -> 0 end)
 
     %__MODULE__{
       advance: advance,
