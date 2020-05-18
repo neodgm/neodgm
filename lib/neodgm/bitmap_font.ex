e56c45f5544aa9b7a0fe7ad1e4845a421c7ec467
Dalgona <dalgona@hontou.moe>
2020-05-18 22:32:30 +0900

    refactor(neodgm): split common glyphs and base glyphs

--- lib/neodgm/bitmap_font.ex
+++ lib/neodgm/bitmap_font.ex
@@ -1,25 +1,30 @@
 defmodule NeoDGM.BitmapFont do
-  base_glyph_sources =
+  common_glyph_sources =
     [
-      BasicLatin,
       BlockElements,
       BoxDrawing,
       BraillePatterns,
       Dingbats,
       EnclosedAlphanumerics,
-      GeneralPunctuation,
       GeometricShapes,
       HangulCompatibilityJamo,
       HangulJamo,
       HangulJamoSource,
       HangulSyllables,
-      Latin1Supplement,
       NumberForms,
-      PowerlineSymbols,
       Trigrams,
-      Components.ShortCapitals,
       Components.Diacritics,
       Components.Fractions
+    ]
+    |> Enum.map(&Module.concat(__MODULE__, &1))
+
+  base_glyph_sources =
+    [
+      BasicLatin,
+      GeneralPunctuation,
+      Latin1Supplement,
+      PowerlineSymbols,
+      Components.ShortCapitals
     ]
     |> Enum.map(&Module.concat(__MODULE__, &1))
 
@@ -36,10 +41,16 @@ defmodule NeoDGM.BitmapFont do
 
   def get_sources("code") do
     [
+      unquote_splicing(common_glyph_sources),
       unquote_splicing(base_glyph_sources),
       unquote_splicing(ligature_glyph_sources)
     ]
   end
 
-  def get_sources(_), do: unquote(base_glyph_sources)
+  def get_sources(_) do
+    [
+      unquote_splicing(common_glyph_sources),
+      unquote_splicing(base_glyph_sources)
+    ]
+  end
 end
