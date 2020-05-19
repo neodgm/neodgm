e1229584453878057b550de53fb76e9006a0e273
Dalgona <dalgona@hontou.moe>
2020-05-20 00:02:36 +0900

    feat(neodgm): modify appearances of enclosed number glyphs

--- lib/neodgm/bitmap_font/enclosed_alphanumerics.ex
+++ lib/neodgm/bitmap_font/enclosed_alphanumerics.ex
@@ -5,326 +5,11 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
   glyph_names = ~w(two three four five six seven eight nine)
 
   export_glyphs do
-    bmp_glyph name: "circle.enclosure" do
-      advance 16
-      xmin 1
-      xmax 15
-      ymin -2
-      ymax 12
-
-      data """
-      00011111111000
-      00100000000100
-      01000000000010
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      01000000000010
-      00100000000100
-      00011111111000
-      """
-    end
-
-    bmp_glyph name: "parens.enclosure" do
-      advance 16
-      xmin 1
-      xmax 15
-      ymin -2
-      ymax 12
-
-      data """
-      00100000000100
-      01000000000010
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      10000000000001
-      01000000000010
-      00100000000100
-      """
-    end
-
-    bmp_glyph name: "period.enclosure" do
-      advance 16
-      xmin 12
-      xmax 14
-      ymin 0
-      ymax 2
-
-      data """
-      11
-      11
-      """
-    end
-
-    bmp_glyph name: "zero.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      011110
-      110011
-      110011
-      110111
-      111111
-      111011
-      110011
-      110011
-      110011
-      011110
-      """
-    end
-
-    bmp_glyph name: "zero.enclosed2" do
-      advance 16
-      xmin 8
-      xmax 13
-      ymin 0
-      ymax 10
-
-      data """
-      01110
-      11011
-      11011
-      11011
-      11011
-      11011
-      11011
-      11011
-      11011
-      01110
-      """
-    end
-
-    bmp_glyph name: "one.enclosed" do
-      advance 16
-      xmin 3
-      xmax 6
-      ymin 0
-      ymax 10
-
-      data """
-      011
-      111
-      011
-      011
-      011
-      011
-      011
-      011
-      011
-      011
-      """
-    end
-
-    bmp_glyph name: "two.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      111110
-      000011
-      000011
-      000011
-      011110
-      110000
-      110000
-      110000
-      110000
-      111111
-      """
-    end
-
-    bmp_glyph name: "two.enclosed2" do
-      advance 16
-      xmin 3
-      xmax 7
-      ymin 0
-      ymax 10
-
-      data """
-      1110
-      0011
-      0011
-      0011
-      0111
-      1100
-      1100
-      1100
-      1100
-      1111
-      """
-    end
-
-    bmp_glyph name: "three.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      111110
-      000011
-      000011
-      000011
-      011110
-      000011
-      000011
-      000011
-      000011
-      111110
-      """
-    end
-
-    bmp_glyph name: "four.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      001110
-      011110
-      110110
-      110110
-      110110
-      111111
-      000110
-      000110
-      000110
-      000110
-      """
-    end
-
-    bmp_glyph name: "five.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      111111
-      110000
-      110000
-      110000
-      111110
-      000011
-      000011
-      000011
-      000011
-      111110
-      """
-    end
-
-    bmp_glyph name: "six.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      011110
-      110000
-      110000
-      110000
-      111110
-      110011
-      110011
-      110011
-      110011
-      011110
-      """
-    end
-
-    bmp_glyph name: "seven.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      111111
-      000011
-      000011
-      000011
-      000110
-      001100
-      001100
-      001100
-      001100
-      001100
-      """
-    end
-
-    bmp_glyph name: "eight.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      011110
-      110011
-      110011
-      110011
-      011110
-      110011
-      110011
-      110011
-      110011
-      011110
-      """
-    end
-
-    bmp_glyph name: "nine.enclosed" do
-      advance 16
-      xmin 5
-      xmax 11
-      ymin 0
-      ymax 10
-
-      data """
-      011110
-      110011
-      110011
-      110011
-      011111
-      000011
-      000011
-      000011
-      000011
-      011110
-      """
-    end
-
     # Circled Numbers
 
     composite_glyph unicode: 0x2460 do
       component {:name, "circle.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:unicode, ?1}, 4, 0
+      component {:name, "one.enclosed"}, 5, 1
     end
 
     0x2461..0x2468
@@ -332,20 +17,20 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
     |> Enum.map(fn {code, name} ->
       composite_glyph unicode: code do
         component {:name, "circle.enclosure"}, 0, 0, flags: [:use_my_metrics]
-        component {:name, name <> ".enclosed"}, 0, 0
+        component {:name, name <> ".enclosed"}, 5, 1
       end
     end)
 
     composite_glyph unicode: 0x2469 do
       component {:name, "circle.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "one.enclosed"}, 0, 0
-      component {:name, "zero.enclosed"}, 2, 0
+      component {:name, "one.enclosed2"}, 3, 1
+      component {:name, "zero.enclosed"}, 7, 1
     end
 
     composite_glyph unicode: 0x246A do
       component {:name, "circle.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "one.enclosed"}, 1, 0
-      component {:name, "one.enclosed"}, 5, 0
+      component {:name, "one.enclosed2"}, 4, 1
+      component {:name, "one.enclosed2"}, 8, 1
     end
 
     0x246B..0x2472
@@ -353,22 +38,22 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
     |> Enum.map(fn {code, name} ->
       composite_glyph unicode: code do
         component {:name, "circle.enclosure"}, 0, 0, flags: [:use_my_metrics]
-        component {:name, "one.enclosed"}, 0, 0
-        component {:name, name <> ".enclosed"}, 2, 0
+        component {:name, "one.enclosed2"}, 3, 1
+        component {:name, name <> ".enclosed"}, 7, 1
       end
     end)
 
     composite_glyph unicode: 0x2473 do
       component {:name, "circle.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "two.enclosed2"}, 0, 0
-      component {:name, "zero.enclosed2"}, 0, 0
+      component {:name, "two.enclosed2"}, 3, 1
+      component {:name, "zero.enclosed2"}, 8, 1
     end
 
     # Parenthesized Numbers
 
     composite_glyph unicode: 0x2474 do
       component {:name, "parens.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:unicode, ?1}, 4, 0
+      component {:name, "one.enclosed"}, 5, 1
     end
 
     0x2475..0x247C
@@ -376,20 +61,20 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
     |> Enum.map(fn {code, name} ->
       composite_glyph unicode: code do
         component {:name, "parens.enclosure"}, 0, 0, flags: [:use_my_metrics]
-        component {:name, name <> ".enclosed"}, 0, 0
+        component {:name, name <> ".enclosed"}, 5, 1
       end
     end)
 
     composite_glyph unicode: 0x247D do
       component {:name, "parens.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "one.enclosed"}, 0, 0
-      component {:name, "zero.enclosed"}, 2, 0
+      component {:name, "one.enclosed2"}, 3, 1
+      component {:name, "zero.enclosed"}, 7, 1
     end
 
     composite_glyph unicode: 0x247E do
       component {:name, "parens.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "one.enclosed"}, 1, 0
-      component {:name, "one.enclosed"}, 5, 0
+      component {:name, "one.enclosed2"}, 4, 1
+      component {:name, "one.enclosed2"}, 8, 1
     end
 
     0x247F..0x2486
@@ -397,15 +82,15 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
     |> Enum.map(fn {code, name} ->
       composite_glyph unicode: code do
         component {:name, "parens.enclosure"}, 0, 0, flags: [:use_my_metrics]
-        component {:name, "one.enclosed"}, 0, 0
-        component {:name, name <> ".enclosed"}, 2, 0
+        component {:name, "one.enclosed2"}, 3, 1
+        component {:name, name <> ".enclosed"}, 7, 1
       end
     end)
 
     composite_glyph unicode: 0x2487 do
       component {:name, "parens.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "two.enclosed2"}, 0, 0
-      component {:name, "zero.enclosed2"}, 0, 0
+      component {:name, "two.enclosed2"}, 3, 1
+      component {:name, "zero.enclosed2"}, 8, 1
     end
 
     # Numbers with a Period
@@ -420,20 +105,20 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
     |> Enum.map(fn {code, name} ->
       composite_glyph unicode: code do
         component {:name, "period.enclosure"}, 0, 0, flags: [:use_my_metrics]
-        component {:name, name <> ".enclosed"}, 0, 0
+        component {:name, name <> ".period"}, 0, 0
       end
     end)
 
     composite_glyph unicode: 9361 do
       component {:name, "period.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "one.enclosed"}, -2, 0
-      component {:name, "zero.enclosed"}, 0, 0
+      component {:name, "one.period"}, -2, 0
+      component {:name, "zero.period"}, 0, 0
     end
 
     composite_glyph unicode: 9362 do
       component {:name, "period.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "one.enclosed"}, -2, 0
-      component {:name, "one.enclosed"}, 3, 0
+      component {:name, "one.period"}, -2, 0
+      component {:name, "one.period"}, 3, 0
     end
 
     0x2493..0x249A
@@ -441,27 +126,27 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
     |> Enum.map(fn {code, name} ->
       composite_glyph unicode: code do
         component {:name, "period.enclosure"}, 0, 0, flags: [:use_my_metrics]
-        component {:name, "one.enclosed"}, -2, 0
-        component {:name, name <> ".enclosed"}, 0, 0
+        component {:name, "one.period"}, -2, 0
+        component {:name, name <> ".period"}, 0, 0
       end
     end)
 
     composite_glyph unicode: 0x249B do
       component {:name, "period.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "two.enclosed2"}, -2, 0
-      component {:name, "zero.enclosed2"}, -2, 0
+      component {:name, "two.period2"}, -2, 0
+      component {:name, "zero.period2"}, -2, 0
     end
 
     # Circled Digit Zero
 
     composite_glyph unicode: 0x24EA do
       component {:name, "circle.enclosure"}, 0, 0, flags: [:use_my_metrics]
-      component {:name, "zero.enclosed"}, 0, 0
+      component {:name, "zero.enclosed"}, 5, 1
     end
 
     # Negative Circled Numbers
 
-    bmp_glyph unicode: 9451 do
+    bmp_glyph unicode: 0x24EB do
       advance 16
       xmin 1
       xmax 15
@@ -469,8 +154,9 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01110011001110
       11100010001111
       11110011001111
@@ -478,15 +164,14 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       11110011001111
       11110011001111
       11110011001111
-      11110011001111
-      11110011001111
       01110011001110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9452 do
+    bmp_glyph unicode: 0x24EC do
       advance 16
       xmin 1
       xmax 15
@@ -494,24 +179,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01100100000110
       11000111110011
       11100111110011
-      11100111110011
       11100110000111
-      11100100111111
       11100100111111
       11100100111111
       11100100111111
       01100100000010
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9453 do
+    bmp_glyph unicode: 0x24ED do
       advance 16
       xmin 1
       xmax 15
@@ -519,24 +204,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01100100000110
       11000111110011
-      11100111110011
       11100111110011
       11100110000111
       11100111110011
       11100111110011
       11100111110011
-      11100111110011
       01100100000110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9454 do
+    bmp_glyph unicode: 0x24EE do
       advance 16
       xmin 1
       xmax 15
@@ -544,24 +229,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01100111000110
       11000110000111
-      11100100100111
       11100100100111
       11100100100111
       11100100000011
       11100111100111
       11100111100111
-      11100111100111
       01100111100110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9455 do
+    bmp_glyph unicode: 0x24EF do
       advance 16
       xmin 1
       xmax 15
@@ -569,24 +254,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01100100000010
       11000100111111
       11100100111111
-      11100100111111
       11100100000111
-      11100111110011
       11100111110011
       11100111110011
       11100111110011
       01100100000110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9456 do
+    bmp_glyph unicode: 0x24F0 do
       advance 16
       xmin 1
       xmax 15
@@ -594,24 +279,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
-      01100110000110
+      01111111111110
+      01100110000010
       11000100111111
-      11100100111111
       11100100111111
       11100100000111
       11100100110011
       11100100110011
       11100100110011
-      11100100110011
       01100110000110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9457 do
+    bmp_glyph unicode: 0x24F1 do
       advance 16
       xmin 1
       xmax 15
@@ -619,24 +304,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01100100000010
       11000111110011
-      11100111110011
       11100111110011
       11100111100111
       11100111001111
       11100111001111
       11100111001111
-      11100111001111
       01100111001110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9458 do
+    bmp_glyph unicode: 0x24F2 do
       advance 16
       xmin 1
       xmax 15
@@ -644,24 +329,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01100110000110
       11000100110011
-      11100100110011
       11100100110011
       11100110000111
       11100100110011
       11100100110011
       11100100110011
-      11100100110011
       01100110000110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9459 do
+    bmp_glyph unicode: 0x24F3 do
       advance 16
       xmin 1
       xmax 15
@@ -669,24 +354,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01100110000110
       11000100110011
-      11100100110011
       11100100110011
       11100110000011
       11100111110011
       11100111110011
       11100111110011
-      11100111110011
       01100110000110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9460 do
+    bmp_glyph unicode: 0x24F4 do
       advance 16
       xmin 1
       xmax 15
@@ -694,24 +379,24 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01000111000110
       11110010010011
       11110010010011
-      11110010010011
-      11100010010011
-      11001110010011
+      11100110010011
       11001110010011
       11001110010011
       11001110010011
       01000011000110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
 
-    bmp_glyph unicode: 9471 do
+    bmp_glyph unicode: 0x24FF do
       advance 16
       xmin 1
       xmax 15
@@ -719,20 +404,20 @@ defmodule NeoDGM.BitmapFont.EnclosedAlphanumerics do
       ymax 12
 
       data """
-      00011111111000
+      00001111110000
       00111111111100
+      01111111111110
       01111000011110
-      11110011001111
       11110011001111
       11110010001111
       11110000001111
       11110001001111
       11110011001111
       11110011001111
-      11110011001111
       01111000011110
+      01111111111110
       00111111111100
-      00011111111000
+      00001111110000
       """
     end
   end
