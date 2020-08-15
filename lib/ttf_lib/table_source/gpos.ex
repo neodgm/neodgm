0c2f052ce3a37677564f10aec2d49cf3fe3491b1
Dalgona <me@dalgona.dev>
2020-08-16 01:35:01 +0900

    fix(ttf_lib): properly sort scripts, languages, and features by tag

--- lib/ttf_lib/table_source/gpos.ex
+++ lib/ttf_lib/table_source/gpos.ex
@@ -23,24 +23,9 @@ defmodule TTFLib.TableSource.GPOS do
   @type feature_indices :: %{optional({<<_::4>>, term()}) => integer()}
   @type lookup_indices :: %{optional(binary()) => integer()}
 
-  @spec populate_indices(t()) :: t()
-  def populate_indices(gpos) do
-    %__MODULE__{
-      gpos
-      | feature_indices: index_map(gpos.feature_list.features, &{&1.tag, &1.name}),
-        lookup_indices: index_map(gpos.lookup_list.lookups, & &1.name)
-    }
-  end
-
-  defp index_map(enumerable, fun) do
-    enumerable
-    |> Enum.with_index()
-    |> Enum.map(fn {item, index} -> {fun.(item), index} end)
-    |> Map.new()
-  end
-
   @spec compile(t()) :: CompiledTable.t()
   def compile(gpos) do
+    gpos = preprocess(gpos)
     offset_base = 14
 
     list_compile_opts = [
@@ -69,5 +54,31 @@ defmodule TTFLib.TableSource.GPOS do
   @spec compile_subtable(map(), integer(), keyword()) :: binary()
   def compile_subtable(subtable, _lookup_type, opts) do
     Subtable.compile(subtable, opts)
+  end
+
+  @spec preprocess(t()) :: t()
+  defp preprocess(gpos) do
+    %__MODULE__{
+      gpos
+      | script_list: ScriptList.sort(gpos.script_list),
+        feature_list: FeatureList.sort(gpos.feature_list)
+    }
+    |> populate_indices()
+  end
+
+  @spec populate_indices(t()) :: t()
+  defp populate_indices(gpos) do
+    %__MODULE__{
+      gpos
+      | feature_indices: index_map(gpos.feature_list.features, &{&1.tag, &1.name}),
+        lookup_indices: index_map(gpos.lookup_list.lookups, & &1.name)
+    }
+  end
+
+  defp index_map(enumerable, fun) do
+    enumerable
+    |> Enum.with_index()
+    |> Enum.map(fn {item, index} -> {fun.(item), index} end)
+    |> Map.new()
   end
 end
--- lib/ttf_lib/table_source/gsub.ex
+++ lib/ttf_lib/table_source/gsub.ex
@@ -16,31 +16,16 @@ defmodule TTFLib.TableSource.GSUB do
           script_list: ScriptList.t(),
           feature_list: FeatureList.t(),
           lookup_list: LookupList.t(),
-          feature_indices: %{optional({<<_::4>>, term()}) => integer()},
-          lookup_indices: %{optional(binary()) => integer()}
+          feature_indices: feature_indices(),
+          lookup_indices: lookup_indices()
         }
 
   @type feature_indices :: %{optional({<<_::4>>, term()}) => integer()}
   @type lookup_indices :: %{optional(binary()) => integer()}
 
-  @spec populate_indices(t()) :: t()
-  def populate_indices(gsub) do
-    %__MODULE__{
-      gsub
-      | feature_indices: index_map(gsub.feature_list.features, &{&1.tag, &1.name}),
-        lookup_indices: index_map(gsub.lookup_list.lookups, & &1.name)
-    }
-  end
-
-  defp index_map(enumerable, fun) do
-    enumerable
-    |> Enum.with_index()
-    |> Enum.map(fn {item, index} -> {fun.(item), index} end)
-    |> Map.new()
-  end
-
   @spec compile(t()) :: CompiledTable.t()
   def compile(gsub) do
+    gsub = preprocess(gsub)
     offset_base = 14
 
     list_compile_opts = [
@@ -69,5 +54,31 @@ defmodule TTFLib.TableSource.GSUB do
   @spec compile_subtable(map(), integer(), keyword()) :: binary()
   def compile_subtable(subtable, _lookup_type, opts) do
     Subtable.compile(subtable, opts)
+  end
+
+  @spec preprocess(t()) :: t()
+  defp preprocess(gsub) do
+    %__MODULE__{
+      gsub
+      | script_list: ScriptList.sort(gsub.script_list),
+        feature_list: FeatureList.sort(gsub.feature_list)
+    }
+    |> populate_indices()
+  end
+
+  @spec populate_indices(t()) :: t()
+  defp populate_indices(gsub) do
+    %__MODULE__{
+      gsub
+      | feature_indices: index_map(gsub.feature_list.features, &{&1.tag, &1.name}),
+        lookup_indices: index_map(gsub.lookup_list.lookups, & &1.name)
+    }
+  end
+
+  defp index_map(enumerable, fun) do
+    enumerable
+    |> Enum.with_index()
+    |> Enum.map(fn {item, index} -> {fun.(item), index} end)
+    |> Map.new()
   end
 end
