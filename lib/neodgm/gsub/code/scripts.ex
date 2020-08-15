7991f65b115c05cad2ac78ba818262390e1a0ec7
Dalgona <me@dalgona.dev>
2020-08-16 01:58:25 +0900

    feat(neodgm_code): enable code ligation for latn{dflt}

--- lib/neodgm/gsub/code/scripts.ex
+++ lib/neodgm/gsub/code/scripts.ex
@@ -8,15 +8,25 @@ defmodule NeoDGM.GSUB.Code.Scripts do
       scripts: [
         %Script{
           tag: "DFLT",
-          default_language: %LanguageSystem{
-            tag: "dflt",
-            features: [
-              {"aalt", "Ligation for program codes"},
-              {"calt", "Ligation for program codes"}
-            ]
-          },
+          default_language: dflt_lang(),
+          languages: []
+        },
+        %Script{
+          tag: "latn",
+          default_language: dflt_lang(),
           languages: []
         }
+      ]
+    }
+  end
+
+  @spec dflt_lang() :: LanguageSystem.t()
+  defp dflt_lang do
+    %LanguageSystem{
+      tag: "dflt",
+      features: [
+        {"aalt", "Ligation for program codes"},
+        {"calt", "Ligation for program codes"}
       ]
     }
   end
