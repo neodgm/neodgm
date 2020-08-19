c0390bddff51cd73aef7caac12ac9cf1dbc1e172
Dalgona <me@dalgona.dev>
2020-08-19 12:32:28 +0900

    feat(neodgm): add substitutions for Hangul jamo

---
+++ lib/neodgm/gsub/hangul/scripts.ex
@@ -0,0 +1,9 @@
+defmodule NeoDGM.GSUB.Hangul.Scripts do
+  alias TTFLib.TableSource.OTFLayout.LanguageSystem
+  alias TTFLib.TableSource.OTFLayout.{Script, ScriptList}
+
+  @spec data() :: ScriptList.t()
+  def data do
+    %ScriptList{scripts: []}
+  end
+end
