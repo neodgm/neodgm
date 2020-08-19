2d86cae01dcacc25bdb3c21ab1788d9ac822f176
Dalgona <me@dalgona.dev>
2020-08-19 11:23:12 +0900

    refactor(neodgm): rename glyph names of Hangul jamo sources

--- lib/neodgm/bitmap_font/hangul_jamo.ex
+++ lib/neodgm/bitmap_font/hangul_jamo.ex
@@ -4,21 +4,27 @@ defmodule NeoDGM.BitmapFont.HangulJamo do
 
   export_glyphs do
     Enum.map(0x1100..0x1112, fn code ->
+      str = (code - 0x1100) |> to_string() |> String.pad_leading(2, "0")
+
       composite_glyph unicode: code do
-        component {:name, "cho_#{code - 0x1100}_0"}, 0, 0
+        component {:name, "cho_0_#{str}"}, 0, 0
       end
     end)
 
     Enum.map(0x1161..0x1175, fn code ->
+      str = (code - 0x1161) |> to_string() |> String.pad_leading(2, "0")
+
       composite_glyph unicode: code do
-        component {:name, "jung_#{code - 0x1161}_0"}, 0, 0
+        component {:name, "jung_0_#{str}"}, 0, 0
       end
     end)
 
     Enum.map(0x11A8..0x11C2, fn code ->
+      # Note that the jongseong index starts with 1.
+      str = (code - 0x11A7) |> to_string() |> String.pad_leading(2, "0")
+
       composite_glyph unicode: code do
-        # Note that the jongseong index starts with 1.
-        component {:name, "jong_#{code - 0x11A7}_0"}, 0, 0
+        component {:name, "jong_0_#{str}"}, 0, 0
       end
     end)
   end
