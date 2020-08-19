de02cb4344b1e5d03e8c580d4543966a781e894e
Dalgona <me@dalgona.dev>
2020-08-19 22:34:26 +0900

    feat(ttf_lib): add custom sigil ~i

--- lib/ttf_lib/util.ex
+++ lib/ttf_lib/util.ex
@@ -1,4 +1,34 @@
 defmodule TTFLib.Util do
+  @spec sigil_i(Macro.t(), keyword()) :: Macro.t()
+  defmacro sigil_i(str_expr, mods)
+  defmacro sigil_i(str_expr, ''), do: do_sigil_i(str_expr, 10)
+  defmacro sigil_i(str_expr, 'o'), do: do_sigil_i(str_expr, 8)
+  defmacro sigil_i(str_expr, 'd'), do: do_sigil_i(str_expr, 10)
+  defmacro sigil_i(str_expr, 'x'), do: do_sigil_i(str_expr, 16)
+
+  @spec sigil_i(Macro.t(), integer()) :: Macro.t()
+  defp do_sigil_i(str_expr, base) do
+    quote(do: unquote(__MODULE__).__sigil_i__(unquote(str_expr), unquote(base)))
+  end
+
+  @spec __sigil_i__(binary(), integer()) :: [integer()]
+  def __sigil_i__(str, base) do
+    str
+    |> String.split(~r/\s+/, trim: true)
+    |> Enum.map(&try_parse_integer(&1, base))
+  end
+
+  @spec try_parse_integer(binary(), integer()) :: integer() | no_return()
+  defp try_parse_integer(str, base) do
+    str
+    |> Integer.parse(base)
+    |> case do
+      {integer, ""} -> integer
+      {_, _} -> raise "#{inspect(str)} is not a valid integer in base #{base}"
+      :error -> raise "#{inspect(str)} is not a valid integer in base #{base}"
+    end
+  end
+
   @spec offsetted_binaries(list(), integer(), (term() -> iodata())) ::
           {integer(), [binary()], [binary()]}
   def offsetted_binaries(sources, offset_base, fun) do
