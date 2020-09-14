9b23d4870b5c850cef3fb458b3a4859921669122
Dalgona <me@dalgona.dev>
2020-09-14 23:25:24 +0900

    build(neodgm): add pixel_font as dependency

--- mix.exs
+++ mix.exs
@@ -13,6 +13,8 @@ defmodule NeoDGM.Mixfile do
   end
 
   defp deps do
-    []
+    [
+      {:pixel_font, github: "Dalgona/pixel_font", ref: "927d2a3491f878670bf32ead40c62321b47d23c1"}
+    ]
   end
 end
