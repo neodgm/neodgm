7c3fb03aaa4550ed471bdd8104f237bfeaba4e09
Dalgona <me@dalgona.dev>
2020-10-08 19:59:28 +0900

    docs: update the copyright and contact information

--- mix.exs
+++ mix.exs
@@ -1,12 +1,10 @@
 defmodule NeoDGM.Mixfile do
   use Mix.Project
 
-  @neodgm_version "1.5.0"
-
   def project do
     [
       app: :neodgm,
-      version: @neodgm_version,
+      version: "1.5.0",
       elixir: "~> 1.9",
       deps: deps()
     ]
