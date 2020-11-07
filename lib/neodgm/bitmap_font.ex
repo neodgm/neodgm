2ee3c990cf42c1f40c9c922fb629185cfcb6ab5b
Dalgona <me@dalgona.dev>
2020-11-07 16:09:53 +0900

    refactor: merge base_glyph_sources into common_glyph_sources

--- lib/neodgm/bitmap_font.ex
+++ lib/neodgm/bitmap_font.ex
@@ -1,33 +1,28 @@
 defmodule NeoDGM.BitmapFont do
   common_glyph_sources =
     [
+      BasicLatin,
       BlockElements,
       BoxDrawing,
       BraillePatterns,
       Dingbats,
       EnclosedAlphanumerics,
       EnclosedCJKLetters,
+      GeneralPunctuation,
       GeometricShapes,
       HalfwidthAndFullwidthForms,
       HangulCompatibilityJamo,
       HangulJamo,
       HangulJamoSource,
       HangulSyllables,
+      Latin1Supplement,
       NumberForms,
+      PowerlineSymbols,
+      SuperscriptsAndSubscripts,
       Trigrams,
       Components.Diacritics,
       Components.Enclosed,
-      Components.Fractions
-    ]
-    |> Enum.map(&Module.concat(__MODULE__, &1))
-
-  base_glyph_sources =
-    [
-      BasicLatin,
-      GeneralPunctuation,
-      Latin1Supplement,
-      PowerlineSymbols,
-      SuperscriptsAndSubscripts,
+      Components.Fractions,
       Components.ShortCapitals
     ]
     |> Enum.map(&Module.concat(__MODULE__, &1))
@@ -46,15 +41,13 @@ defmodule NeoDGM.BitmapFont do
   def get_sources("code") do
     [
       unquote_splicing(common_glyph_sources),
-      unquote_splicing(base_glyph_sources),
       unquote_splicing(ligature_glyph_sources)
     ]
   end
 
   def get_sources(_) do
     [
-      unquote_splicing(common_glyph_sources),
-      unquote_splicing(base_glyph_sources)
+      unquote_splicing(common_glyph_sources)
     ]
   end
 end
