30f88616828e6a0d0936a1495daee577a252bdde
Dalgona <me@dalgona.dev>
2020-11-07 16:05:12 +0900

    feat: add short capital letters for Latin Extended-A glyphs

--- lib/neodgm/bitmap_font/components/short_capitals.ex
+++ lib/neodgm/bitmap_font/components/short_capitals.ex
@@ -18,6 +18,40 @@ glyph_source NeoDGM.BitmapFont.Components.ShortCapitals do
     """
   end
 
+  bmp_glyph "C.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    0111110
+    1100011
+    1100000
+    1100000
+    1100000
+    1100000
+    1100000
+    1100011
+    0111110
+    """
+  end
+
+  bmp_glyph "D.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    1111110
+    1100011
+    1100011
+    1100011
+    1100011
+    1100011
+    1100011
+    1100011
+    1111110
+    """
+  end
+
   bmp_glyph "E.short" do
     advance 8
     bounds 0..7, 0..9
@@ -35,6 +69,40 @@ glyph_source NeoDGM.BitmapFont.Components.ShortCapitals do
     """
   end
 
+  bmp_glyph "G.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    0111110
+    1100011
+    1100000
+    1100000
+    1100111
+    1100011
+    1100011
+    1100011
+    0111111
+    """
+  end
+
+  bmp_glyph "H.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    1100011
+    1100011
+    1100011
+    1100011
+    1111111
+    1100011
+    1100011
+    1100011
+    1100011
+    """
+  end
+
   bmp_glyph "I.short" do
     advance 8
     bounds 1..7, 0..9
@@ -49,6 +117,40 @@ glyph_source NeoDGM.BitmapFont.Components.ShortCapitals do
     001100
     001100
     111111
+    """
+  end
+
+  bmp_glyph "J.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    0001111
+    0000110
+    0000110
+    0000110
+    0000110
+    0000110
+    0000110
+    1100110
+    0111100
+    """
+  end
+
+  bmp_glyph "L.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    1100000
+    1100000
+    1100000
+    1100000
+    1100000
+    1100000
+    1100000
+    1100000
+    1111111
     """
   end
 
@@ -86,6 +188,57 @@ glyph_source NeoDGM.BitmapFont.Components.ShortCapitals do
     """
   end
 
+  bmp_glyph "R.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    1111110
+    1100011
+    1100011
+    1100011
+    1111110
+    1100011
+    1100011
+    1100011
+    1100011
+    """
+  end
+
+  bmp_glyph "S.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    0111110
+    1100011
+    1100000
+    1100000
+    0111110
+    0000011
+    0000011
+    1100011
+    0111110
+    """
+  end
+
+  bmp_glyph "T.short" do
+    advance 8
+    bounds 1..7, 0..9
+
+    data """
+    111111
+    001100
+    001100
+    001100
+    001100
+    001100
+    001100
+    001100
+    001100
+    """
+  end
+
   bmp_glyph "U.short" do
     advance 8
     bounds 0..7, 0..9
@@ -99,6 +252,23 @@ glyph_source NeoDGM.BitmapFont.Components.ShortCapitals do
     1100011
     1100011
     1100011
+    0111110
+    """
+  end
+
+  bmp_glyph "W.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    1101011
+    1101011
+    1101011
+    1101011
+    1101011
+    1101011
+    1101011
+    1101011
     0111110
     """
   end
@@ -117,6 +287,23 @@ glyph_source NeoDGM.BitmapFont.Components.ShortCapitals do
     001100
     001100
     001100
+    """
+  end
+
+  bmp_glyph "Z.short" do
+    advance 8
+    bounds 0..7, 0..9
+
+    data """
+    1111111
+    0000011
+    0000011
+    0000110
+    0011100
+    0110000
+    1100000
+    1100000
+    1111111
     """
   end
 end
