479360ebfb22435e3f48b72cd253444665a57bda
Dalgona <me@dalgona.dev>
2020-11-21 10:14:35 +0900

    build: bump Elixir version requirement to "~> 1.11"

--- mix.exs
+++ mix.exs
@@ -5,14 +5,14 @@ defmodule NeoDGM.Mixfile do
     [
       app: :neodgm,
       version: "1.5.10",
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       deps: deps()
     ]
   end
 
   defp deps do
     [
-      {:pixel_font, github: "Dalgona/pixel_font", ref: "01c8a0f800888884741aa80b265c2bd664306ec9"}
+      {:pixel_font, github: "Dalgona/pixel_font", ref: "7bec009833d1cccea35dccf8dad5f45b3077aecc"}
     ]
   end
 end
