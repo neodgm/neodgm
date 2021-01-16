5e56504fcf01603d1320382238916099082d91f8
Dalgona <me@dalgona.dev>
2021-01-16 17:37:07 +0900

    feat: specify feature/script/language in lookups

--- lib/neodgm/lookups/gsub/code/contexts.ex
+++ lib/neodgm/lookups/gsub/code/contexts.ex
@@ -1,7 +1,13 @@
 use PixelFont.OTFLayout
 
 lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
+  module do
+    defp scripts, do: %{"DFLT" => [:default], "latn" => [:default]}
+  end
+
   lookup :chained_context, "Short bidirectional arrow chain" do
+    feature "calt", scripts()
+
     context do
       backtrack '<'
       input '-=', apply: "Short bidirectional arrow body"
@@ -10,6 +16,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :chained_context, "Left arrow head chain" do
+    feature "calt", scripts()
+
     context do
       backtrack '<'
       input '-', apply: "Left arrow head"
@@ -23,6 +31,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :chained_context, "Right arrow head chain" do
+    feature "calt", scripts()
+
     context do
       backtrack ~w(hyphen.larr.head equal.larr.head)
       backtrack '<'
@@ -37,6 +47,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :chained_context, "Left arrow body chain" do
+    feature "calt", scripts()
+
     context do
       backtrack ~w(hyphen.larr.head equal.larr.head)
       backtrack '<'
@@ -52,6 +64,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :chained_context, "Bidirectional arrow joiner chain" do
+    feature "calt", scripts()
+
     context do
       backtrack ~w(hyphen.larr.head hyphen.larr.body equal.larr.head equal.larr.body)
       input '-=', apply: "Bidirectional arrow joiner"
@@ -61,6 +75,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :chained_context, "Left arrow tail chain" do
+    feature "calt", scripts()
+
     context do
       backtrack ~w(hyphen.larr.head hyphen.larr.body equal.larr.head equal.larr.body)
       input '-=', apply: "Left arrow body"
@@ -68,6 +84,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :reverse_chaining_context, "Right arrow body chain" do
+    feature "calt", scripts()
+
     context do
       lookahead ~w(hyphen.rarr.head hyphen.rarr.body equal.rarr.head equal.rarr.body)
       substitute ?-, "hyphen.rarr.body"
@@ -76,6 +94,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :chained_context, "Markup comment chain" do
+    feature "calt", scripts()
+
     context do
       backtrack '<'
       input '!', apply: "Markup comment exclam"
@@ -85,6 +105,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :chained_context, "Pipe operator chain" do
+    feature "calt", scripts()
+
     context do
       input '<', apply: "Pipe operator"
       input '|', apply: "Pipe operator"
@@ -107,6 +129,8 @@ lookups NeoDGM.Lookups.GSUB.Code.Contexts, for: "GSUB" do
   end
 
   lookup :reverse_chaining_context, "Right pipe operator chain" do
+    feature "calt", scripts()
+
     context do
       lookahead ~w(bar.pipeoperator)
       substitute ?|, "bar.pipeoperator"
