a78e321b8d158214e5ddb51b8c9e383f9b257a4d
Dalgona <me@dalgona.dev>
2021-02-13 12:01:01 +0900

    refactor: move Hangul component lists to NeoDGM.BitmapFont

--- lib/neodgm/lookups/gpos/hangul.ex
+++ lib/neodgm/lookups/gpos/hangul.ex
@@ -2,35 +2,32 @@ use PixelFont.OTFLayout
 
 lookups NeoDGM.Lookups.GPOS.Hangul, for: "GPOS" do
   module do
+    import NeoDGM.BitmapFont,
+      only: [
+        all_cho_glyphs: 0,
+        all_jung_glyphs: 0,
+        all_jong_glyphs: 0
+      ]
+
     defp scripts do
       %{"DFLT" => [:default], "hang" => [:default], "jamo" => [:default]}
     end
-
-    glyph_names = fn prefix, set_range, num_range ->
-      for set <- set_range, num <- num_range do
-        "#{prefix}_#{set}_#{num |> to_string() |> String.pad_leading(2, "0")}"
-      end
-    end
-
-    @all_cho glyph_names.("cho", 0..7, 0..18)
-    @all_jung glyph_names.("jung", 0..3, 0..20)
-    @all_jong glyph_names.("jong", 0..3, 1..27)
   end
 
   lookup :single_adjustment, "Overlap jungseong letters" do
-    adjust_uniform @all_jung, x_placement: -16, x_advance: -16
+    adjust_uniform all_jung_glyphs(), x_placement: -16, x_advance: -16
   end
 
   lookup :single_adjustment, "Overlap jongseong letters" do
-    adjust_uniform @all_jong, x_placement: -16, x_advance: -16
+    adjust_uniform all_jong_glyphs(), x_placement: -16, x_advance: -16
   end
 
   lookup :chained_context, "Hangul LVT form, jungseong" do
     feature "vjmo", scripts()
 
     context do
-      backtrack @all_cho
-      input @all_jung, apply: "Overlap jungseong letters"
+      backtrack all_cho_glyphs()
+      input all_jung_glyphs(), apply: "Overlap jungseong letters"
     end
   end
 
@@ -38,9 +35,9 @@ lookups NeoDGM.Lookups.GPOS.Hangul, for: "GPOS" do
     feature "tjmo", scripts()
 
     context do
-      backtrack @all_jung
-      backtrack @all_cho
-      input @all_jong, apply: "Overlap jongseong letters"
+      backtrack all_jung_glyphs()
+      backtrack all_cho_glyphs()
+      input all_jong_glyphs(), apply: "Overlap jongseong letters"
     end
   end
 end
