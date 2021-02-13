8ec8cb9dd5ec112cb4719435d086c4d39a930514
Dalgona <me@dalgona.dev>
2021-02-13 12:01:01 +0900

    refactor: move Hangul component lists to NeoDGM.BitmapFont

--- lib/neodgm/bitmap_font.ex
+++ lib/neodgm/bitmap_font.ex
@@ -58,4 +58,18 @@ defmodule NeoDGM.BitmapFont do
       unquote_splicing(base_glyph_sources)
     ]
   end
+
+  jamo_glyph_names = fn prefix, set_range, index_range ->
+    for set <- set_range, index <- index_range do
+      "#{prefix}_#{set}_#{index |> to_string() |> String.pad_leading(2, "0")}"
+    end
+  end
+
+  @all_cho_glyphs jamo_glyph_names.("cho", 0..7, 0..18)
+  @all_jung_glyphs jamo_glyph_names.("jung", 0..3, 0..20)
+  @all_jong_glyphs jamo_glyph_names.("jong", 0..3, 1..27)
+
+  def all_cho_glyphs, do: @all_cho_glyphs
+  def all_jung_glyphs, do: @all_jung_glyphs
+  def all_jong_glyphs, do: @all_jong_glyphs
 end
